1c94f16f745a6138a4c420c2b5f769d461b44449
Jeroen Domburg <Spritetm@users.noreply.github.com>
2023-10-18 08:44:08 +0000

    Add CI checks for file sanity (#119)

---
+++ check-file-format.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Check a file in this repo for sanity. Used for CI to quickly highlight
+# errors that aren't easy to spot in a diff (e.g. 0xB101 instead of 0x8101)
+# Written in Bash because of reasons.
+
+#Check command line args. We need two.
+if [ -z "$2" ]; then
+	echo "Simple pid file sanity checker thing"
+	echo "Usage: $0 file_to_check starting_pid"
+	exit 1
+fi
+
+#Read into file descriptor 6.
+exec 6< $1
+
+#skip header
+while read l <&6 && ! [ "$l" = 'PID    | Product name' ]; do
+	:
+done
+
+#See if the lines a. have a '| desc' bit that is not zero, and if they
+#start with the next PID in order.
+pid=$(($2))
+while read l <&6; do
+	if [ -n "$l" ]; then
+		#Split the line into pid and desc
+		rpid="${l% |*}"
+		desc="${l#*| }"
+
+		#Check if there is a description. This also errors out when there is no | char.
+		if [ -z "$desc" ]; then
+			echo "Malformed line: '$l'"
+			exit 1
+		fi
+
+		# Check PID validity
+		expect=`printf "0x%04X" $pid`
+		if [ "$rpid" != "$expect" ]; then
+			echo "Malformed line: '$l'"
+			echo "Expected pid $expect, got pid $rpid"
+			exit 1
+		fi
+		#OK, next PID.
+		pid=$((pid+1));
+	fi
+done
+
+#See if we parsed any lines at all.
+if [ "$pid" = $(($2)) ]; then
+	echo "Error: No entries in file?"
+	exit 1;
+fi
+
+#All OK.
+exit 0
+
